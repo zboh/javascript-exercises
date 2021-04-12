417004df2c24812118d519002d47e1831b403f4e
Herman Sabo <sabo@hermans-mbp.lan>
2021-04-12 15:00:06 +0400

    solved palimdrome exercise

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,4 +1,15 @@
-const palindromes = function() {
+const palindromes = function(str) {
+	const regex = /[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g;
+	const whiteSpace = /\s/g;
+	const removePunct = str.replace(regex, '');
+	const removeWhiteSpace = removePunct.replace(whiteSpace, '');
+	const lowerCase = removeWhiteSpace.toLowerCase();
+	const reversed = lowerCase
+										.split('')
+										.reverse()
+										.join('');
+	return lowerCase === reversed;
+
 
 }
 
