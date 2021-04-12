9d365cf2bcb5f43be41d3cfbfd25020b3c62a1c2
Herman Sabo <sabo@hermans-mbp.lan>
2021-04-12 14:31:21 +0400

    solved calculator exercise

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,25 +1,33 @@
-function add () {
+function add (a, b) {
+	return a + b;
 	
 }
 
-function subtract () {
+function subtract (a, b) {
+	return a - b;
 	
 }
 
-function sum () {
+function sum (arr) {
+		return arr.reduce((sum, int) => sum + int, 0);
+}
+
+function multiply (arr) {
+	return arr.reduce((acc, current) => acc * current, 1);
 	
 }
 
-function multiply () {
-	
+function power(a, b) {
+	return Math.pow(a, b)
 }
 
-function power() {
-	
-}
-
-function factorial() {
-	
+function factorial(num) {
+	if (num === 0 || num === 1)
+    return 1;
+  for (var i = num - 1; i >= 1; i--) {
+    num *= i;
+  }
+  return num;
 }
 
 module.exports = {
