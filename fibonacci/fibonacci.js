ec405fddcdcd81196d8b5f19e4b2b5c722a711e3
Herman Sabo <sabo@Hermans-MacBook-Pro.local>
2021-04-14 23:30:26 +0400

    solved fibonacci exercise.

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+	let toInteger = parseInt(num);
+	let first = 0;
+	let second = 1;
+	let sum = 0;
+	if(toInteger < 0) {
+		return 'OOPS';
+	}
 
+	if(toInteger == 0 || toInteger == 1) {
+		return toInteger;
+	}
+
+	for (let i = 1; i < toInteger; i++) {
+		sum = first + second;
+		first = second;
+		second = sum;
+	}
+	return sum;
 }
+
+
 
 module.exports = fibonacci
