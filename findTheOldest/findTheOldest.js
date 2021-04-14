2ad197573b7c699665e1bc633d0bbbd485b82bae
Herman Sabo <sabo@Hermans-MacBook-Pro.local>
2021-04-15 00:25:38 +0400

    stuck..

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-let findTheOldest = function() {
-
+let findTheOldest = function(arr) {
+	let oldest = arr.reduce((age, person) => {
+		if('yearOfDeath' in person) {
+			return;
+		} else {
+			person['yearOfDeath'] = new Date().getFullYear();
+		};
+		age = person['yearOfDeath'] - person['yearOfDeath'];
+	}, 0);
 }
 
 module.exports = findTheOldest
+
+
